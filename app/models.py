143abdc174238e230244bbe64c24823a089e36f5
Mikhail Bukharov <mikhail.bukharov@gmail.com>
2024-11-20 13:37:50 +0300

    comments

--- app/models.py
+++ app/models.py
@@ -1,7 +1,7 @@
 from app import db, login_manager
 from flask_login import UserMixin # Этот класс даёт возможность работать с пользователем
 
-@login_manager.user_loader
+@login_manager.user_loader  # Этот декоратор связывает функцию с flask_login, чтобы загружать пользователя по id
 def load_user(user_id):
     return User.query.get(int(user_id))
 
